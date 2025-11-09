ce0771c055e384eda37d1987e01ae0b657bfeb88
Alrashim <alrashimawal5@gmail.com>
2025-11-09 19:53:35 +0800

    guide book modified

--- constants/presets.ts
+++ constants/presets.ts
@@ -5,14 +5,14 @@ export interface Preset {
 }
 
 export const PRESETS: Preset[] = [
-  { id: 1, name: "Brush My Teeth", image: require("../assets/images/brushing.gif") },
-  { id: 2, name: "Let's Eat", image: require("../assets/images/eating.gif") },
-  { id: 3, name: "Bath Time", image: require("../assets/images/washing.gif") },
-  { id: 4, name: "Dress Up Time", image: require("../assets/images/putting clothes.gif") },
-  { id: 5, name: "Go to School", image: require("../assets/images/going to school.gif") },
-  { id: 6, name: "Go to Home", image: require("../assets/images/going home.gif") },
-  { id: 7, name: "Bedtime Prep", image: require("../assets/images/putting pajama.gif") },
-  { id: 8, name: "Go to Sleep", image: require("../assets/images/going to sleep.gif") },
+  { id: 1, name: "Brush My Teeth", image: require("../assets/gifs/brushing.gif") },
+  { id: 2, name: "Let's Eat", image: require("../assets/gifs/eating.gif") },
+  { id: 3, name: "Bath Time", image: require("../assets/gifs/washing.gif") },
+  { id: 4, name: "Dress Up Time", image: require("../assets/gifs/putting clothes.gif") },
+  { id: 5, name: "Go to School", image: require("../assets/gifs/going to school.gif") },
+  { id: 6, name: "Go to Home", image: require("../assets/gifs/going home.gif") },
+  { id: 7, name: "Bedtime Prep", image: require("../assets/gifs/putting pajama.gif") },
+  { id: 8, name: "Go to Sleep", image: require("../assets/gifs/going to sleep.gif") },
 ];
 
 export const getPresetById = (id?: number | null): Preset | undefined => {
