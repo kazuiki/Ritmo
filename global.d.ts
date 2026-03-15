6de88d0decc238931058b0199559d9b4fbbe68c7
unknown <deato.johncarlo.antalan@gmail.com>
2026-03-16 00:07:55 +0800

    fix: removed global.d.ts that causes an errors

--- global.d.ts
+++
@@ -1,6 +0,0 @@
-declare module '@react-native-async-storage/async-storage';
-declare module 'expo-av';
-declare module 'expo-notifications';
-declare module 'react-native';
-
-declare var require: any;
